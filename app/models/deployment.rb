031f6ce6dfbe339720ab3a21ce2b7438460f7c2b
TJ Taylor <dugancathal@gmail.com>
2016-03-02 02:41:15 -0700

    Redirect properly on deployment delete

--- app/models/deployment.rb
+++ app/models/deployment.rb
@@ -1,7 +1,6 @@
 class Deployment < ActiveRecord::Base
   belongs_to :user, foreign_key: :owner_id
   def repo
-    puts user.token, repo_id.class, repo_id.inspect
     @repo ||= github.repo(repo_id.to_i)
   end
 
